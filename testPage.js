5d5af2ce8da3264d667e4179f862e709281bf7e0
Alfred Yang <alfred.yang@aelf.io>
2019-08-17 20:41:05 +0800

    perf: ⚡️ use a more efficient way to get firstMeaningfulPaint

--- testPage.js
+++ testPage.js
@@ -1,6 +1,7 @@
 const {
   extractDataFromPerformanceMetrics,
-  extractDataFromPerformanceTiming
+  extractDataFromPerformanceTiming,
+  getTimeFromPerformanceMetrics
 } = require('./helpers');
 
 async function testPage(page) {
@@ -8,20 +9,29 @@ async function testPage(page) {
     const client = await page.target().createCDPSession();
     await client.send('Performance.enable');
 
-    await page.setViewport({ width: 1920, height: 1080 });
-    // await page.goto('https://summit.aelf.io');
-    await page.goto('http://192.168.199.216:5000 ');
-    // await page.evaluate(() => {
-    //   document.documentElement.scrollTop = 1000;
-    // });
-    // await page.screenshot({ path: './assets/example.png' })
+    // It sees that the size of viewport will slow down the puppeteer's spped
+    // await page.setViewport({ width: 1920, height: 1080 });
+    // await page.goto('https://summit.aelf.io/test.html');
+    // await page.goto('http://192.168.199.216:5000/test.html');
+    await page.goto('https://baidu.com');
 
-    await page.waitFor(1000); // todo: emmmmm?
     const performanceTiming = JSON.parse(
       await page.evaluate(() => JSON.stringify(window.performance.timing))
     );
-    const performanceMetrics = await client.send('Performance.getMetrics');
 
+    let firstMeaningfulPaint = 0;
+    let performanceMetrics;
+    let runTimes = 0;
+    while (firstMeaningfulPaint === 0) {
+      await page.waitFor(300);
+      performanceMetrics = await client.send('Performance.getMetrics');
+      firstMeaningfulPaint = getTimeFromPerformanceMetrics(
+        performanceMetrics,
+        'FirstMeaningfulPaint'
+      );
+      runTimes++;
+      if (runTimes === 30) break;
+    }
     const metricsA = extractDataFromPerformanceMetrics(
       performanceMetrics,
       'FirstMeaningfulPaint'
